ec539c949af8f0d6ae52298179274b31b09dd58c
JIMICODE <1159081156@qq.com>
2017-05-06 17:54:24 +0800

    Some functions.

--- DXD12_FRAMEWORK/FrameResource.h
+++ DXD12_FRAMEWORK/FrameResource.h
@@ -5,6 +5,35 @@
 #include"d3dUtil.h"
 #include"UploadBuffer.h"
 
+struct ObjectConstants
+{
+	DirectX::XMFLOAT4X4  World = MathHelper::Identity4X4();
+};
+
+struct PassConstants
+{
+	DirectX::XMFLOAT4X4 View = MathHelper::Identity4X4();
+	DirectX::XMFLOAT4X4 InvView = MathHelper::Identity4X4();
+	DirectX::XMFLOAT4X4 Proj = MathHelper::Identity4X4();
+	DirectX::XMFLOAT4X4 InvProj = MathHelper::Identity4X4();
+	DirectX::XMFLOAT4X4 ViewProj = MathHelper::Identity4X4();
+	DirectX::XMFLOAT4X4 InvViewProj = MathHelper::Identity4X4();
+	DirectX::XMFLOAT3 EyePosW = { 0.0f, 0.0f, 0.0f };
+	float cbPerObjectPad1 = 0.0f;
+	DirectX::XMFLOAT2 RenderTargetSize = { 0.0f, 0.0f };
+	DirectX::XMFLOAT2 InvRenderTargetSize = { 0.0f, 0.0f };
+	float NearZ = 0.0f;
+	float FarZ = 0.0f;
+	float TotalTime = 0.0f;
+	float DeltaTime = 0.0f;
+};
+
+struct Vertex
+{
+	DirectX::XMFLOAT3 Pos;
+	DirectX::XMFLOAT4 Color;
+};
+
 struct FrameResource
 {
 public:
@@ -19,7 +48,12 @@ public:
 
 	//we cannot update a cbuffer until the GPU is done processing the
 	//commands that reference it. SO each frame needs their own cbuffers.
-	std::unique_ptr<UploadBuffer<PassConstan>>
+	std::unique_ptr<UploadBuffer<PassConstants>> PassCB = nullptr;
+	std::unique_ptr<UploadBuffer<ObjectConstants>> ObjectCB = nullptr;
+
+	//Fence value to mark commands up to this fence point. This lets us
+	//check if these frame resource are still in use by the GPU
+	UINT64 Fence = 0;
 };
 
 #endif // !FRAMERESOURCE_H_
---
+++ DXD12_FRAMEWORK/ShapeApp.cpp
@@ -0,0 +1,4 @@
+#include"d3dapp.h"
+#include"MathHelper.h"
+#include"UploadBuffer.h"
+#include"FrameResource.h"
